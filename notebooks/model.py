343357f860a083ad44cbfbe0025883fbe495fa7a
ManifoldFR <wilson.jallet@gmail.com>
2018-11-02 13:51:17 +0100

    Push work from before holiday

--- notebooks/model.py
+++ notebooks/model.py
@@ -1,5 +1,7 @@
 """
-Models for the Neural Hawkes process.
+Point process models.
+
+@author: manifold
 """
 import torch
 from torch import nn
@@ -35,12 +37,8 @@ class NeuralCTLSTM(nn.Module):
         # we can learn the parameters of this
         self.decay_act = nn.Softplus()
 
-        # The hidden state contains
-        # the cell state at t, the target cell state
-        self.init_hidden()
-
         self.activation = nn.Softplus()
-        self.weight_f = torch.rand(self.hidden_dim, device=device)
+        self.weight_a = torch.rand(self.hidden_dim, device=device)
 
     def init_hidden(self, batch_size=1):
         """
@@ -48,33 +46,25 @@ class NeuralCTLSTM(nn.Module):
         cells c and cbar.
         The first dimension is the batch size.
         """
-        self.hidden = (torch.rand(batch_size, self.hidden_dim, device=device),
-                       torch.rand(batch_size, self.hidden_dim, device=device),
-                       torch.rand(batch_size, self.hidden_dim, device=device))
-
-    def c_func(self, dt: torch.Tensor, c: torch.Tensor,
-               cbar: torch.Tensor, decay: torch.Tensor):
-        """
-        Compute the decayed cell memory c(t) = c(ti + dt)
-        """
-        # print("Computing decayed cell memory...")
-        # print(c.shape, type(c))
-        # print(cbar.shape, type(cbar))
-        # print(decay.shape, type(decay))
-        # print(dt, type(dt))
-        dt_expd = dt.unsqueeze(-1).expand(c.shape)
-        return cbar + (c - cbar) * torch.exp(-decay * dt_expd)
+        return (torch.randn(batch_size, self.hidden_dim, device=device),
+                torch.randn(batch_size, self.hidden_dim, device=device),
+                torch.randn(batch_size, self.hidden_dim, device=device))
 
     def next_event(self, output, dt, decay):
-        # h_ti, c_ti, cbar = self.hidden
         # c_t_after = self.c_func(dt, c_ti, cbar, decay)
         # h_t_after = output * torch.tanh(c_t_after)
         # lbdaMax = h_t_after
         raise NotImplementedError
 
-    def forward(self, inter_times):
+    def forward(self, inter_times, h_ti, c_ti, c_target_i):
         """
         inter_times: inter-arrival time for the next event in the sequence
+
+        Args:
+            inter_times:    interarrival times
+            h_ti:           prev. hidden state
+            c_ti:           prev. cell state
+            c_target_i:     prev. cell state target
 
         Returns:
             output : result of the output gate
@@ -84,9 +74,6 @@ class NeuralCTLSTM(nn.Module):
             decay_t: decay parameter on the interval
         #TODO event type embedding
         """
-        # get the hidden state and memory from before
-        h_ti, c_ti, cbar = self.hidden
-
         # TODO concatenate event embedding with ht
         v = torch.cat((h_ti,))
         input = torch.sigmoid(self.input_g(v))
@@ -104,47 +91,55 @@ class NeuralCTLSTM(nn.Module):
 
         # Now update the cell memory
         # Decay the cell memory
-        c_t_after = self.c_func(inter_times, c_ti, cbar, decay_t)
+        # c_t_actual = self.c_func(inter_times, c_ti, c_target_i, decay_t)
         # Update the cell
-        c_ti = forget * c_t_after + input * zi
-        # Update the cell state asymptotic value
-        cbar = forget_target * cbar + input_target * zi
-        h_ti = output * torch.tanh(c_t_after)
+        c_ti = forget * c_ti + input * zi
+        # Update the cell state target
+        c_target_i = forget_target * c_target_i + input_target * zi
+        c_t_actual = (
+            c_target_i + (c_ti - c_target_i) *
+            torch.exp(-decay_t*inter_times.unsqueeze(1))
+        )
+        # h_t_actual = output * torch.tanh(c_t_actual)
+        h_ti = output * torch.tanh(c_ti)
 
         # Store our new states for the next pass to use
-        self.hidden = h_ti, c_ti, cbar
-        return output, h_ti, c_ti, cbar, decay_t
+        return output, h_ti, c_ti, c_t_actual, c_target_i, decay_t
 
     def eval_intensity(self, dt: torch.Tensor, output: torch.Tensor,
-                       c_ti, cbar, decay):
+                       c_ti, c_target_i, decay) -> torch.Tensor:
         """
         Compute the intensity function
         Args:
-            dt:     time increments array
-                    dt[i] is the time elapsed since event t_i
-                    verify that if you want to compute at time t,
-                    t_i <= t <= t_{i+1}, then dt[i] = t - t_i
-            output: NN output o_i
-            c_ti:   previous cell state
-            cbar:   previous cell target
-            decay:  decay[i] is the degrowth param. on range [t_i, t_{i+1}]
+            dt  time increments array
+                dt[i] is the time elapsed since event t_i
+                verify that if you want to compute at time t,
+                t_i <= t <= t_{i+1}, then dt[i] = t - t_i
+            output  NN output o_i
+            c_ti    previous cell state
+            c_target_i  previous cell target
+            decay   decay[i] is the degrowth param. on range [t_i, t_{i+1}]
 
         It is best to store the training history in variables for this.
         """
         # Get the updated c(t)
-        c_t_after = self.c_func(dt, c_ti, cbar, decay)
+        c_t_after = (
+            c_target_i + (c_ti - c_target_i) *
+            torch.exp(-decay * dt.unsqueeze(-1).expand(c_ti.shape))
+        )
+
         h_t = output * torch.tanh(c_t_after)
         batch_size = h_t.size(0)
         try:
-            hidden_size = self.weight_f.size(0)
-            weight_f = (
-                self.weight_f.expand(batch_size, hidden_size).unsqueeze(1)
+            hidden_size = self.weight_a.size(0)
+            weight_a = (
+                self.weight_a.expand(batch_size, hidden_size).unsqueeze(1)
             )
-            pre_lambda = torch.bmm(weight_f, h_t.transpose(2, 1)).squeeze(1)
+            pre_lambda = torch.bmm(weight_a, h_t.transpose(2, 1)).squeeze(1)
         except BaseException:
             print("Error occured in c_func")
             print(" dt shape %s" % str(dt.shape))
-            print(" Weights shape %s" % str(self.weight_f.shape))
+            print(" Weights shape %s" % str(self.weight_a.shape))
             print(" h_t shape %s" % str(h_t.shape))
 
             raise
@@ -159,27 +154,33 @@ class NeuralCTLSTM(nn.Module):
         output: entire output history
         decay:  entire decay history
         """
-        inter_times = event_times[:, -1:] - event_times[:, 1:]
+        inter_times: torch.Tensor = event_times[:, -1:] - event_times[:, 1:]
         # Get the intensity process
         event_intensities = self.eval_intensity(
             inter_times, output_hist,
             cell_hist, cell_target_hist, decay_hist)
         first_sum = event_intensities.log().sum(dim=1)
         # The integral term is computed using a Monte Carlo method
-        batch_size = event_times.size(0)
-        input_length = event_times.size(1)
-        # random samples in [0, T]
-        # dim (batch_size) * (input_length)
-        # each sample belongs to
-        samples: torch.Tensor = inter_times * torch.rand_like(inter_times)
+        M = 10  # Monte Carlo samples on each dimension
+        samples: torch.Tensor = (
+            inter_times.expand(M, *inter_times.shape) *
+            torch.rand(M, *inter_times.shape)
+        )
 
-        # Get 
-        lam_samples = self.eval_intensity(
-            samples, output_hist,
-            cell_hist, cell_target_hist, decay_hist)
-        integral = lam_samples.sum(dim=1)
+        # Get the samples of the intensity function
+        lam_samples = torch.stack([self.eval_intensity(
+            samples[i], output_hist, cell_hist,
+            cell_target_hist, decay_hist) for i in range(M)]).mean(dim=0)
+        integral = torch.sum(inter_times*lam_samples, dim=1)
         # Tensor of dim. batch_size
         # of the values of the likelihood
         res = first_sum - integral
         # return the opposite of the mean
-        return -res.mean()
+        return - res.mean()
+
+    def pred_loss(self, output, cell_hist, cell_target_hist):
+        #
+        pass
+
+    def generate_seq(self, cell_hist, cell_target_hist):
+        pass
