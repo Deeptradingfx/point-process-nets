085b10b85deb71c030827e64c03631b6cf45444b
ManifoldFR <wilson.jallet@gmail.com>
2018-11-03 19:47:18 +0100

    update train loop and likelihood func

--- notebooks/model.py
+++ notebooks/model.py
@@ -30,19 +30,18 @@ class NeuralCTLSTM(nn.Module):
         # Cell decay factor
         self.decay = nn.Linear(hidden_dim, hidden_dim)
         # we can learn the parameters of this
-        self.decay_act = nn.Softplus()
-        self.activation = nn.Softplus()
+        self.decay_act = nn.Softplus(beta=6.)
+        self.activation = nn.Softplus(beta=6.)
         self.weight_a = torch.rand(self.hidden_dim, device=device)
 
     def init_hidden(self, batch_size=1):
         """
-        Initialize the hidden state, and the two hidden memory
-        cells c and cbar.
+        Initialize the hidden state, the cell state and cell state target.
         The first dimension is the batch size.
         """
-        return (torch.randn(batch_size, self.hidden_dim, device=device),
-                torch.randn(batch_size, self.hidden_dim, device=device),
-                torch.randn(batch_size, self.hidden_dim, device=device))
+        return (torch.zeros(batch_size, self.hidden_dim, device=device),
+                torch.zeros(batch_size, self.hidden_dim, device=device),
+                torch.zeros(batch_size, self.hidden_dim, device=device))
 
     def next_event(self, output, dt, decay):
         # c_t_after = self.c_func(dt, c_ti, cbar, decay)
@@ -50,49 +49,47 @@ class NeuralCTLSTM(nn.Module):
         # lbdaMax = h_t_after
         raise NotImplementedError
 
-    def forward(self, inter_times, h_ti, c_ti, c_target_i):
+    def forward(self, dt, hidden_i, cell_i, c_target_i):
         """
         Forward pass for the CT-LSTM.
 
         Args:
-            inter_times: event inter-arrival times
-            h_ti: prev. hidden state
-            c_ti: prev. cell state
+            dt: event inter-arrival times
+            hidden_i: prev. hidden state
+            cell_i: prev. cell state
             c_target_i: prev. cell state target
 
         Returns:
             output: result of the output gate
-            h_ti: hidden state
-            c_ti: cell state
-            cbar: cell target
-            decay_t: decay parameter on the interval
-        #TODO event type embedding
+            hidden_i: hidden state
+            cell_i: cell state
+            c_target_i: cell target
+            decay_i: decay parameter on the interval
+        TODO event type embedding
         """
         # TODO concatenate event embedding with ht
-        v = torch.cat((h_ti,))
+        v = torch.cat((hidden_i,))
         input = torch.sigmoid(self.input_g(v))
         forget = torch.sigmoid(self.forget_g(v))
         output = torch.sigmoid(self.output_g(v))
-
         input_target = torch.sigmoid(self.ibar(v))
         forget_target = torch.sigmoid(self.fbar(v))
-
         # Not-quite-c
         z_i = torch.tanh(self.z_gate(v))
         # Compute the decay parameter
-        decay_t = self.decay_act(self.decay(v))
+        decay_i = self.decay_act(self.decay(v))
         # Update the cell
-        c_ti = forget * c_ti + input * z_i
+        cell_i = forget * cell_i + input * z_i
         # Update the cell state target
         c_target_i = forget_target * c_target_i + input_target * z_i
         c_t_actual = (
-            c_target_i + (c_ti - c_target_i) *
-            torch.exp(-decay_t*inter_times)
+            c_target_i + (cell_i - c_target_i) *
+            torch.exp(-decay_i*dt)
         )
         # h_t_actual = output * torch.tanh(c_t_actual)
-        h_ti = output * torch.tanh(c_ti)
+        hidden_i = output * torch.tanh(cell_i)
         # Return our new states for the next pass to use
-        return output, h_ti, c_ti, c_t_actual, c_target_i, decay_t
+        return output, hidden_i, cell_i, c_t_actual, c_target_i, decay_i
 
     def eval_intensity(self, dt: torch.Tensor, output: torch.Tensor,
                        c_ti, c_target_i, decay) -> torch.Tensor:
@@ -102,16 +99,19 @@ class NeuralCTLSTM(nn.Module):
         Args:
             dt: time increments array
                 dt[i] is the time elapsed since event t_i
-                verify that if you want to compute at time t,
-                t_i <= t <= t_{i+1}, then dt[i] = t - t_i
+                verify that if you want to compute at time s,
+                t_i <= s <= t_{i+1}, then dt[i] = s - t_i
             output: NN output o_i
             c_ti: previous cell state
             c_target_i: previous cell target
             decay: decay[i] is the degrowth param. on range [t_i, t_{i+1}]
 
+        Shape:
+            batch_size * max_seq_length * hidden_dim
+
         It is best to store the training history in variables for this.
         """
-        # Get the updated c(t)
+        # Get the values of c(s)
         c_t_after = (
             c_target_i + (c_ti - c_target_i) *
             torch.exp(-decay * dt.unsqueeze(-1).expand(c_ti.shape))
@@ -137,8 +137,10 @@ class NeuralCTLSTM(nn.Module):
             c_ti: entire cell state history
             output: entire output history
             decay: entire decay history
+        
+        Shape:
+            []
         """
-        # pdb.set_trace()
         max_seq_length = event_times.size(0)
         inter_times = event_times[1:] - event_times[:-1]
         # Get the intensity process
@@ -147,26 +149,34 @@ class NeuralCTLSTM(nn.Module):
             cell_hist, cell_target_hist, decay_hist)
         log_sum = event_lambdas.log().sum()
         # The integral term is computed using a Monte Carlo method
-        samples: torch.Tensor = (
-            T*torch.rand(*event_times.shape)
+        n_samples = event_times.shape[0]
+        all_samples: torch.Tensor = (
+            T*torch.rand(*event_times.shape, n_samples)
         )
-        samples, _ = samples.sort(0)
-        dsamples = samples[1:] - samples[:-1]
-        # Get the index of each elt of samples inside
-        # the subdivision given by the event_times array
-        indices = (
-            torch.sum(samples[:-1,None] >= event_times, dim=1)
-            - (max_seq_length-seq_lengths)
-        )
-        # Get the samples of the intensity function
-        try:
-            lam_samples = self.eval_intensity(
-                dsamples, output_hist[:,indices,:],
-                cell_hist[:,indices,:], cell_target_hist[:,indices,:],
-                decay_hist[:,indices,:])
-        except:
-            pdb.set_trace()
-        integral = torch.sum(inter_times*lam_samples)
+        all_samples, _ = all_samples.sort(0)
+        lam_samples_ = []
+        for i in range(n_samples):
+            samples = all_samples[:, i]
+            dsamples = samples[1:] - samples[:-1]
+            # Get the index of each elt of samples inside
+            # the subdivision given by the event_times array
+            # we have to substract 1 (indexes start at 1)
+            # and the no. of trailing, padding 0s in the sequence
+            indices = (
+                torch.sum(samples[:-1,None] >= event_times[:-1], dim=1)
+                - (max_seq_length-seq_lengths) - 1
+            )
+            # Get the samples of the intensity function
+            try:
+                lam_samples_.append(self.eval_intensity(
+                    dsamples, output_hist[:,indices,:],
+                    cell_hist[:,indices,:], cell_target_hist[:,indices,:],
+                    decay_hist[:,indices,:]))
+            except Exception as inst:
+                pdb.set_trace()
+                raise
+        lam_samples_ = torch.stack(lam_samples_)
+        integral = torch.sum(inter_times*lam_samples_, dim=1)
         # Tensor of dim. batch_size
         # of the values of the likelihood
         res = -log_sum + integral
