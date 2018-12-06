f5dd52733407501acdb743f1e6835391e6b140a2
ManifoldFR <wilson.jallet@gmail.com>
2018-12-06 23:22:55 +0100

    Empty saved models

--- models/ctlstm.py
+++ models/ctlstm.py
@@ -26,7 +26,7 @@ class HawkesLSTMCell(nn.Module):
         # Cell decay factor, identical for all hidden dims
         self.decay_layer = nn.Sequential(
             nn.Linear(input_dim + hidden_size, hidden_size),
-            nn.Softplus(beta=1.))
+            nn.Softplus(beta=3.))
 
     def forward(self, x, h_t, c_t, c_target) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
         """
@@ -83,7 +83,7 @@ class HawkesLSTM(nn.Module):
         # activation for the intensity
         self.intensity_layer = nn.Sequential(
             nn.Linear(hidden_size, self.process_dim, bias=True),  # no bias in the model
-            nn.Softplus(beta=1.))
+            nn.Softplus(beta=3.))
 
     def init_hidden(self, batch_size: int = 1, device=None) -> Tuple[Tensor, Tensor, Tensor]:
         """
@@ -265,7 +265,6 @@ class HawkesLSTMGen(SeqGenerator):
             s = torch.zeros(1)
             h0, c0, _ = model.init_hidden()
             h0.normal_(std=0.1)
-            h0 += 1.
             c0.normal_(std=0.1)
             h_t = h0
             c_t = c0
