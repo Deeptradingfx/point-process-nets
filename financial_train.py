8febb36d625116c8b27b04d9f44d67f63338ca84
ManifoldFR <wilson.jallet@gmail.com>
2018-12-10 02:01:41 +0100

    add saving to train script

--- financial_train.py
+++ financial_train.py
@@ -1,8 +1,11 @@
+import os
+import datetime
 import torch
 from torch import nn, optim
 import numpy as np
 from models import HawkesDecayRNN
-from train_functions import train_decayrnn
+from train_functions import train_decayrnn, plot_loss
+from utils.save_model import save_model
 import pandas as pd
 from argparse import ArgumentParser
 
@@ -13,9 +16,9 @@ parser.add_argument('--cuda', action='store_true',
                     help="Whether or not to use GPU acceleration.")
 
 args = parser.parse_args()
-DATA_FILE = args.data  # data path
+chosen_file = args.data  # data path
 
-df = pd.read_csv(DATA_FILE)
+df = pd.read_csv(chosen_file)
 df = df[df.OrderType != 0]  # remove cancel orders
 day_stamp = df.Date.values[0]
 print("Day:", day_stamp)
@@ -24,7 +27,7 @@ print(df)
 
 # Arrange our data
 evt_times = df.Time.values
-evt_types = (df.OrderType.values + 1)//2  # 0 is ask, 1 is bid
+evt_types = (df.OrderType.values + 1) // 2  # 0 is ask, 1 is bid
 
 num_of_splits = 1000
 split_times_list = np.array_split(evt_times, num_of_splits)
@@ -42,18 +45,40 @@ if args.cuda:
     seq_types = seq_types.cuda()
 
 # Define model
-PROCESS_DIM = 2
-HIDDEN_SIZE = 128
+process_dim = 2
+hidden_size = 128
 
-EPOCHS = 4
-BATCH_SIZE = 32
-
-model = HawkesDecayRNN(PROCESS_DIM, HIDDEN_SIZE)
+model = HawkesDecayRNN(process_dim, hidden_size)
+MODEL_NAME = model.__class__.__name__
 if args.cuda:
     model = model.cuda()
 num_of_paramters = sum(e.numel() for e in model.parameters())
 print("no. of model parameters:", num_of_paramters)
-optimizer = optim.Adam(model.parameters(), lr=5e-3)
 
+# Train model
+EPOCHS = 4
+BATCH_SIZE = 32
+
+optimizer = optim.Adam(model.parameters(), lr=5e-3)
 loss = train_decayrnn(model, optimizer, seq_times, seq_types, seq_lengths, -1,  # tmax actually doesn't matter
                       BATCH_SIZE, EPOCHS, use_cuda=args.cuda)
+
+# Model file dump
+SAVED_MODELS_PATH = os.path.abspath('saved_models')
+os.makedirs(SAVED_MODELS_PATH, exist_ok=True)
+# print("Saved models directory: {}".format(SAVED_MODELS_PATH))
+
+date_format = "%Y%m%d-%H%M%S"
+now_timestamp = datetime.datetime.now().strftime(date_format)
+extra_tag = "{}d".format(process_dim)
+filename_base = "{}-{}_hidden{}-{}".format(
+    MODEL_NAME, extra_tag,
+    hidden_size, now_timestamp)
+
+save_model(model, chosen_file, extra_tag,
+           hidden_size, now_timestamp, MODEL_NAME)
+
+# Plot loss and save
+filename_loss_plot = "loss_plot_" + filename_base + ".png"
+fig = plot_loss(EPOCHS, loss)
+fig.savefig(filename_loss_plot)
