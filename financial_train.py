3d01c64629bb72f55a1f8789bcfc2d1fac24065d
ManifoldFR <wilson.jallet@gmail.com>
2018-12-10 19:41:46 +0100

    update

--- financial_train.py
+++ financial_train.py
@@ -9,6 +9,8 @@ from utils.save_model import save_model
 import pandas as pd
 from argparse import ArgumentParser
 
+pd.options.mode.chained_assignment = None
+
 DEFAULT_LR = 1e-3
 DEFAULT_BATCH = 32
 
@@ -32,36 +34,33 @@ data_files = args.data  # data path
 data_files.sort()
 
 # Data processing
-frames_ = []
-for file in data_files:
-    df_ = pd.read_csv(file)
-    day_stamp = df_.Date.values[0]
-    print("Day", day_stamp)
-    frames_.append(df_)
-
-df = pd.concat(frames_, ignore_index=True)
-
-df['MidPrice'] = 0.5*(df.AskPriceAfter + df.BidPriceAfter)
-df['MidPriceVar'] = df.MidPrice.diff()
-df_filtered = df[df.MidPriceVar != 0]
-df_filtered['MidPriceChangeType'] = (df.MidPriceVar > 0).astype(int)
-
-# Arrange our data
-evt_times = df_filtered.Time.values
-evt_types = df_filtered.MidPriceChangeType.values
-
-print("Sequence length:", len(evt_times))
+split_times_list = []
+split_types_list = []
+seq_lengths = []
 num_of_splits = int(input("Number of splits: "))
-print("Split sequence lengths:", len(evt_times)/num_of_splits)
 
-split_times_list = np.array_split(evt_times, num_of_splits)
-split_types_list = np.array_split(evt_types, num_of_splits)
-seq_lengths = [len(e) for e in split_times_list]
-split_times_list = [torch.from_numpy(e) for e in split_times_list]
-split_types_list = [torch.from_numpy(e) for e in split_types_list]
+for file in data_files:
+    df = pd.read_csv(file)
+    day_stamp = df.Date.values[0]
+    print("Day", day_stamp)
+    df['MidPrice'] = 0.5*(df.AskPriceAfter + df.BidPriceAfter)
+    df['MidPriceVar'] = df.MidPrice.diff()
+    df_filtered: pd.DataFrame = df[df.MidPriceVar != 0]
+    df_filtered['MidPriceChangeType'] = (df.MidPriceVar > 0).astype(int)
+    # Arrange our data
+    evt_times = df_filtered.Time.values
+    evt_types = df_filtered.MidPriceChangeType.values
+
+    sttl_ = np.array_split(evt_times, num_of_splits)
+    stpl_ = np.array_split(evt_types, num_of_splits)
+    seq_lengths += [len(e) for e in sttl_]
+    split_times_list += [torch.from_numpy(e) for e in sttl_]
+    split_types_list += [torch.from_numpy(e) for e in stpl_]
 seq_lengths = torch.LongTensor(seq_lengths) - 1
 seq_times = nn.utils.rnn.pad_sequence(split_times_list, batch_first=True).to(torch.float32)
 seq_types = nn.utils.rnn.pad_sequence(split_types_list, batch_first=True)
+print(seq_times.shape)
+
 
 if args.cuda:
     seq_lengths = seq_lengths.cuda()
